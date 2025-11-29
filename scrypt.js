e95724a6ef6244cc00c323c86efd3e8b5362e7b9
toswa <sgricenko259@gmail.com>
2025-11-29 17:23:19 +0200

    Implement product fetching and display in grid
    Fetches product data from a JSON server and displays it in a grid format.

--- scrypt.js
+++ scrypt.js
@@ -1,1 +1,37 @@
 
+const API_URL = 'https://my-json-server.typicode.com/sns-toswa/one/products';
+
+const grid = document.getElementById('products-grid');
+
+async function getProducts() {
+    try {
+        const response = await fetch(API_URL);
+        const products = await response.json();
+
+        // Очищаємо грін перед додаванням
+        grid.innerHTML = '';
+
+        products.forEach(product => {
+            // Створюємо картку HTML
+            const card = document.createElement('div');
+            card.classList.add('card');
+
+            card.innerHTML = `
+                <h3>${product.title}</h3>
+                <img src="${product.image}" alt="${product.title}">
+                <div class="price">Price: ${product.price}$</div>
+                <p>${product.description}</p>
+                <a href="#" class="seller-link">Seller profile</a>
+                <button class="buy-btn">Buy</button>
+            `;
+
+            grid.appendChild(card);
+        });
+
+    } catch (error) {
+        console.error('Помилка завантаження:', error);
+    }
+}
+
+// Запускаємо функцію
+getProducts();
